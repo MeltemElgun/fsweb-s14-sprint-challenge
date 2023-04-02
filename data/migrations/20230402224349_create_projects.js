ebe9d4bd2356ee0786eba4584aa7301aebf8db0f
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-04-03 02:05:35 +0300

    fsweb-s14-sprint-challenge.Completed.

---
+++ data/migrations/20230402224349_create_projects.js
@@ -0,0 +1,67 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return (
+    knex.schema
+      .createTable("projects", (tbl) => {
+        tbl.increments("project_id");
+        tbl.string("project_name").notNullable();
+        tbl.string("project_description");
+        tbl.boolean("project_completed").defaultTo(false);
+      })
+      .createTable("resources", (tbl) => {
+        tbl.increments("resource_id");
+        tbl.string("resource_name").notNullable().unique();
+        tbl.string("resource_description");
+      })
+      .createTable("tasks", (tbl) => {
+        tbl.increments("task_id");
+        tbl.string("task_description").notNullable();
+        tbl.string("task_notes");
+        tbl.boolean("task_completed").defaultTo(false);
+        //ilişki kısmı
+        tbl
+          .integer("project_id")
+          .notNullable()
+          .unsigned()
+          .references("project_id")
+          .inTable("projects")
+          .onUpdate("CASCADE") //"RESTRICT"
+          .onDelete("CASCADE");
+      })
+      //many to many ilişkisini kuran tablo.
+      .createTable("project_resources", (tbl) => {
+        tbl.increments("project_resources_id");
+        tbl
+          .integer("project_id")
+          .notNullable()
+          .unsigned()
+          .references("project_id")
+          .inTable("projects")
+          .onUpdate("CASCADE") //"RESTRICT"
+          .onDelete("CASCADE");
+        tbl
+          .integer("resource_id")
+          .notNullable()
+          .unsigned()
+          .references("resource_id")
+          .inTable("resources")
+          .onUpdate("CASCADE") //"RESTRICT"
+          .onDelete("CASCADE");
+      })
+  );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
